caophamtruongson/cucumber

gherkin: (C) Always print feature file data using UTF-8.
Windows does not use UTF-8 by default. To make sure that UTF-8 is used
in the output, do manual conversion from unicode/UTF-16 to an UTF-8
byte sequence for output. It seems safer that to try to add Windows
specific code to set UTF-8 output on that platform.

gherkin/c/src/print_utilities.c
@@ -1,22 +1,30 @@
 #include "print_utilities.h"
+#include "unicode_utilities.h"
 
 void PrintUtilities_print_json_string(FILE* file, const wchar_t* text) {
     int i;
     for (i = 0; i < wcslen(text); ++i) {
         if (text[i] == L'\\' || text[i] == L'"') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)text[i]);
+            fputc((char)'\\', file);
+            fputc((char)text[i], file);
         }
         else if (text[i] == L'\n') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)L'n');
+            fputc((char)'\\', file);
+            fputc((char)'n', file);
         }
         else if (text[i] == L'\r') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)L'r');
+            fputc((char)'\\', file);
+            fputc((char)'r', file);
         }
         else {
-            fprintf(file, "%lc", (wint_t)text[i]);
+            i = UnicodeUtilities_print_wide_character_to_utf8_file(file, text, i);
         }
+    }
+}
+
+void PrintUtilities_wide_string(FILE* file, const wchar_t* text) {
+    int i;
+    for (i = 0; i < wcslen(text); ++i) {
+        i = UnicodeUtilities_print_wide_character_to_utf8_file(file, text, i);
     }
 }

gherkin/c/src/print_utilities.h
@@ -10,6 +10,8 @@ extern "C" {
 
 void PrintUtilities_print_json_string(FILE* file, const wchar_t* text);
 
+void PrintUtilities_wide_string(FILE* file, const wchar_t* text);
+
 #ifdef __cplusplus
 }
 #endif

gherkin/c/src/token_formatter_builder.c
@@ -1,4 +1,5 @@
 #include "token_formatter_builder.h"
+#include "print_utilities.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -40,31 +41,41 @@ void TokenFormatterBuilder_build(Builder* builder, Token* token) {
         fprintf(((TokenFormatterBuilder*)builder)->file, "%s\n", token_type_to_string(token->matched_type));
     else if (token->matched_type == Token_TableRow || token->matched_type == Token_TagLine) {
         fprintf(((TokenFormatterBuilder*)builder)->file,
-                "(%d:%d)%s:%ls/%ls/",
+                "(%d:%d)%s:",
                 token->location.line,
                 token->location.column,
-                token_type_to_string(token->matched_type),
-                token->matched_keyword ? token->matched_keyword : L"",
-                token->matched_text ? token->matched_text : L"");
+                token_type_to_string(token->matched_type));
+        PrintUtilities_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_keyword ? token->matched_keyword : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
+        PrintUtilities_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_text ? token->matched_text : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
         int i;
         for (i = 0; i < token->matched_items->count; ++i) {
             if (i != 0)
                 fprintf(((TokenFormatterBuilder*)builder)->file, ",");
             fprintf(((TokenFormatterBuilder*)builder)->file,
-                    "%d:%ls",
-                    token->matched_items->items[i].column,
-                    token->matched_items->items[i].text);
+                    "%d:",
+                    token->matched_items->items[i].column);
+            PrintUtilities_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                       token->matched_items->items[i].text);
         }
         fprintf(((TokenFormatterBuilder*)builder)->file, "\n");
     }
-    else
+    else {
         fprintf(((TokenFormatterBuilder*)builder)->file,
-                "(%d:%d)%s:%ls/%ls/\n",
+                "(%d:%d)%s:",
                 token->location.line,
                 token->location.column,
-                token_type_to_string(token->matched_type),
-                token->matched_keyword ? token->matched_keyword : L"",
-                token->matched_text ? token->matched_text : L"");
+                token_type_to_string(token->matched_type));
+        PrintUtilities_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_keyword ? token->matched_keyword : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
+        PrintUtilities_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_text ? token->matched_text : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/\n");
+    }
     Token_delete(token);
 }
 

cucumber/gherkin

c/src/print_utilities.c
@@ -1,22 +1,30 @@
 #include "print_utilities.h"
+#include "unicode_utilities.h"
 
 void PrintUtilities_print_json_string(FILE* file, const wchar_t* text) {
     int i;
     for (i = 0; i < wcslen(text); ++i) {
         if (text[i] == L'\\' || text[i] == L'"') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)text[i]);
+            fputc((char)'\\', file);
+            fputc((char)text[i], file);
         }
         else if (text[i] == L'\n') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)L'n');
+            fputc((char)'\\', file);
+            fputc((char)'n', file);
         }
         else if (text[i] == L'\r') {
-            fprintf(file, "%lc", (wint_t)L'\\');
-            fprintf(file, "%lc", (wint_t)L'r');
+            fputc((char)'\\', file);
+            fputc((char)'r', file);
         }
         else {
-            fprintf(file, "%lc", (wint_t)text[i]);
+            i = UnicodeUtilities_print_wide_character_to_utf8_file(file, text, i);
         }
+    }
+}
+
+void PrintUtilities_print_wide_string(FILE* file, const wchar_t* text) {
+    int i;
+    for (i = 0; i < wcslen(text); ++i) {
+        i = UnicodeUtilities_print_wide_character_to_utf8_file(file, text, i);
     }
 }

c/src/print_utilities.h
@@ -10,6 +10,8 @@ extern "C" {
 
 void PrintUtilities_print_json_string(FILE* file, const wchar_t* text);
 
+void PrintUtilities_print_wide_string(FILE* file, const wchar_t* text);
+
 #ifdef __cplusplus
 }
 #endif

c/src/token_formatter_builder.c
@@ -1,4 +1,5 @@
 #include "token_formatter_builder.h"
+#include "print_utilities.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -40,31 +41,41 @@ void TokenFormatterBuilder_build(Builder* builder, Token* token) {
         fprintf(((TokenFormatterBuilder*)builder)->file, "%s\n", token_type_to_string(token->matched_type));
     else if (token->matched_type == Token_TableRow || token->matched_type == Token_TagLine) {
         fprintf(((TokenFormatterBuilder*)builder)->file,
-                "(%d:%d)%s:%ls/%ls/",
+                "(%d:%d)%s:",
                 token->location.line,
                 token->location.column,
-                token_type_to_string(token->matched_type),
-                token->matched_keyword ? token->matched_keyword : L"",
-                token->matched_text ? token->matched_text : L"");
+                token_type_to_string(token->matched_type));
+        PrintUtilities_print_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_keyword ? token->matched_keyword : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
+        PrintUtilities_print_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_text ? token->matched_text : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
         int i;
         for (i = 0; i < token->matched_items->count; ++i) {
             if (i != 0)
                 fprintf(((TokenFormatterBuilder*)builder)->file, ",");
             fprintf(((TokenFormatterBuilder*)builder)->file,
-                    "%d:%ls",
-                    token->matched_items->items[i].column,
-                    token->matched_items->items[i].text);
+                    "%d:",
+                    token->matched_items->items[i].column);
+            PrintUtilities_print_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                       token->matched_items->items[i].text);
         }
         fprintf(((TokenFormatterBuilder*)builder)->file, "\n");
     }
-    else
+    else {
         fprintf(((TokenFormatterBuilder*)builder)->file,
-                "(%d:%d)%s:%ls/%ls/\n",
+                "(%d:%d)%s:",
                 token->location.line,
                 token->location.column,
-                token_type_to_string(token->matched_type),
-                token->matched_keyword ? token->matched_keyword : L"",
-                token->matched_text ? token->matched_text : L"");
+                token_type_to_string(token->matched_type));
+        PrintUtilities_print_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_keyword ? token->matched_keyword : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/");
+        PrintUtilities_print_wide_string(((TokenFormatterBuilder*)builder)->file,
+                                   token->matched_text ? token->matched_text : L"");
+        fprintf(((TokenFormatterBuilder*)builder)->file, "/\n");
+    }
     Token_delete(token);
 }
 