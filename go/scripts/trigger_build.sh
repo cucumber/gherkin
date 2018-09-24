f870d5622dbca847fd19b725ebc3e16a7bd007f7
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-09-24 21:37:16 +0100

    Build tweaks

--- go/scripts/trigger_build.sh
+++ go/scripts/trigger_build.sh
@@ -1,34 +1,18 @@
 #!/usr/bin/env bash
 #
-# Triggers a build of a repo
+# Triggers a tagged build of a module repo, cancelling any started or running
+# builds first.
 #
 set -euf -o pipefail
 
-# foo/bar => foo%2Fbar
 org=$1
 repo=$2
 tag=$3
 token=$4
 org_repo="${org}%2F${repo}"
-# body_fmt='{
-#   "request": {
-#     "tag":"%s",
-#     "message": "Trigger build of tag %s"
-#   }
-# }'
-# 
-# body=$(printf "${body_fmt}" "${tag}" "${tag}")
 
-# curl -v -s -X POST \
-#    -H "Content-Type: application/json" \
-#    -H "Accept: application/json" \
-#    -H "Travis-API-Version: 3" \
-#    -H "Authorization: token ${token}" \
-#    -d "${body}" \
-#    https://api.travis-ci.org/repo/${repo}/requests
-
-
-build=$(curl \
+# Get the latest builds
+builds=$(curl \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -H "Travis-API-Version: 3" \
@@ -36,13 +20,14 @@ build=$(curl \
     "https://api.travis-ci.org/repo/${org_repo}/builds"
 )
 
-build_id=$(echo "${build}" | jq "[.builds[] | select(.tag.name == \"${tag}\")][0] | .id")
+# Find the id of the build with the git tag we're interested in
+build_id=$(echo "${builds}" | jq "[.builds[] | select(.tag.name == \"${tag}\")][0] | .id")
+
+# Find the build's state
 build_state=$(echo "${build}" | jq --raw-output "[.builds[] | select(.tag.name == \"${tag}\")][0] | .state")
 
-echo "build_state=${build_state}"
-
 if [ "$build_state" = "started" || "$build_state" = "created" ]; then
-    echo "Cancelling..."
+    echo "Cancelling ${build_state} build of ${org}/${repo}@${tag}"
     curl -X POST \
         -H "Content-Type: application/json" \
         -H "Accept: application/json" \
@@ -51,7 +36,7 @@ if [ "$build_state" = "started" || "$build_state" = "created" ]; then
         "https://api.travis-ci.org/build/${build_id}/cancel"
 fi
 
-echo "Restarting..."
+echo "Restarting build of ${org}/${repo}@${tag}"
 curl -X POST \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
