6ceda8ef37f0af8527612e910bbcc27217e0d618
M.P. Korstanje <rien.korstanje@gmail.com>
2022-10-31 20:33:00 +0100

    Clean up dotnet makefile

--- dotnet/default.mk
+++ dotnet/default.mk
@@ -14,21 +14,6 @@ rwildcard=$(foreach d,$(wildcard $(1:=/*)),$(call rwildcard,$d,$2) $(filter $(su
 default: .packed
 .PHONY: default
 
-update-dependencies:
-	@echo -e "\033[0;31mPlease update dependencies for dotnet manually!!\033[0m"
-.PHONY: update-dependencies
-
-pre-release: update-version update-dependencies clean default
-.PHONY: pre-release
-
-update-version:
-ifdef NEW_VERSION
-	./scripts/update-version
-else
-	@echo -e "\033[0;31mNEW_VERSION is not defined. Can't update version :-(\033[0m"
-	exit 1
-endif
-
 .built: $(SLN_FILES) $(CSPROJ_FILES) $(CSHARP_SOURCE_FILES) .generated
 	dotnet build -c Release -p:SnapshotSuffix="$(SNAPSHOT_SUFFIX)"
 	touch $@
@@ -43,25 +28,11 @@ endif
 .packed: .tested
 	touch $@
 
-# Define SNAPSHOT_SUFFIX to make pre-release: export SNAPSHOT_SUFFIX=beta-1 make publish
-publish: .packed
-ifdef NUGET_API_KEY
-	# https://circleci.com/gh/cucumber/cucumber/edit#env-vars
-	@dotnet nuget push --source https://api.nuget.org/v3/index.json --api-key "${NUGET_API_KEY}" $(shell find */bin/Release/NuGet -name "*.$(NEW_VERSION).nupkg")
-else
-	@echo -e "\033[0;31mNUGET_API_KEY is not defined. Can't publish :-(\033[0m"
-	exit 1
-endif
-
-post-release:
-	@echo "No post-release needed for dotnet"
-.PHONY: post-release
-
 clean: clean-dotnet
 .PHONY: clean
 
 clean-dotnet:
-	rm -rf .generated .tested .built .packed GeneratedNuGetPackages
+	rm -rf .generated .tested .built .packed
 	rm -rf */bin
 	rm -rf */obj
 .PHONY: clean-dotnet
