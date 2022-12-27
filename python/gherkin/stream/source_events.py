c0b94675e5ecb37d55e2c3613567b6e8bd4d2647
temyers <temyers@users.noreply.github.com>
2022-12-27 17:03:43 +0800

    feat: Python - implement mediaType for markdown features

--- python/gherkin/stream/source_events.py
+++ python/gherkin/stream/source_events.py
@@ -5,10 +5,17 @@ def source_event(path):
         'source': {
             'uri': path,
             'data': io.open(path, 'r', encoding='utf8', newline='').read(),
-            'mediaType': 'text/x.cucumber.gherkin+plain'
+            'mediaType': _media_type(path)
         }
     }
     return event
+
+
+def _media_type(path):
+    if(path.endswith(".feature")):
+        return 'text/x.cucumber.gherkin+plain'
+    if(path.endswith(".feature.md")):
+        return 'text/x.cucumber.gherkin+markdown'
 
 class SourceEvents:
     def __init__(self, paths):
