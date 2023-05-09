0cce844ec049759b7d6dfc9b8f51ab581c816a62
Remy Chibois <rcs@weez-u-welding.com>
2023-05-09 18:21:02 +0200

    chore: adding binaries

---
+++ cpp/src/bin/gherkin/gherkin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <gherkin/parser.hpp>
+
+struct options
+{
+    bool print_source_events;
+    bool print_ast_events;
+    bool print_pickle_events;
+};
+
+int main(int ac, char** av)
+{
+    return 0;
+}
