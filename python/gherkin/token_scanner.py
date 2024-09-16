d3643982676b4ba0a5fc8f5fd8aed41ff31b5f80
Alessio Bogon <778703+youtux@users.noreply.github.com>
2024-09-16 21:26:51 +0200

    Add type annotations for token_scanner

--- python/gherkin/token_scanner.py
+++ python/gherkin/token_scanner.py
@@ -16,20 +16,20 @@ class TokenScanner:
     :file:`gherkin-languages.json`.
     """
 
-    def __init__(self, path_or_str):
+    def __init__(self, path_or_str: str) -> None:
         if os.path.exists(path_or_str):
             self.io = open(path_or_str, encoding='utf8')
         else:
             self.io = io.StringIO(path_or_str)
         self.line_number = 0
 
-    def read(self):
+    def read(self) -> Token:
         self.line_number += 1
         location = {'line': self.line_number}
         line = self.io.readline()
         return Token((GherkinLine(line, self.line_number) if line else line), location)
 
-    def __del__(self):
+    def __del__(self) -> None:
         # close file descriptor if it's still open
         try:
             self.io.close()
