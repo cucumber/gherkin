003222490218d96f160cf72c6d305e63e39dcdcd
Luke Hill <lukehill_uk@hotmail.com>
2024-10-28 12:15:11 +0000

    Simplify TokenScanner#read

--- ruby/lib/gherkin/token_scanner.rb
+++ ruby/lib/gherkin/token_scanner.rb
@@ -26,8 +26,10 @@ module Gherkin
 
     def read
       location = { line: @line_number += 1 }
-      if @io.nil? || line = @io.gets
-        gherkin_line = line ? GherkinLine.new(line, location[:line]) : nil
+      if @io.nil?
+        Token.new(nil, location)
+      elsif line = @io.gets
+        gherkin_line = GherkinLine.new(line, location[:line])
         Token.new(gherkin_line, location)
       else
         @io.close unless @io.closed? # ARGF closes the last file after final gets
