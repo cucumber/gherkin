65c525177a731e62fd2cda99998bef0e48ef65d5
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2025-03-28 17:04:58 +0100

    Optimize GherkinDialect performance (#380)
    Separating out the elements from https://github.com/cucumber/gherkin/pull/372
    that involve the GherkinDialect
    
    * Optimize memory size of `stepKeywords`
    * Use `EnumSet` for `stepKeywordsTypes`
    * Adds `getDistinctStepKeywordTypes` that returns the `EnumSet`
    * Deprecates `getStepKeywordTypes`
    
    Co-authored-by: U117293 <U117293@mobi.ch>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ This project adheres to [Semantic Versioning](http://semver.org).
 This document is formatted according to the principles of [Keep A CHANGELOG](http://keepachangelog.com).
 
 ## [Unreleased]
+### Changed
+- [Java] Optimize GherkinDialect performance ([#380](https://github.com/cucumber/gherkin/pull/380))
 
 ## [32.0.1] - 2025-03-27
 ### Fixed
--- java/src/test/java/io/cucumber/gherkin/GherkinDialectProviderTest.java
+++ java/src/test/java/io/cucumber/gherkin/GherkinDialectProviderTest.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static io.cucumber.gherkin.StringUtils.symbolCount;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GherkinDialectProviderTest {
+    
     @Test
     public void provides_emoji_dialect() {
         GherkinDialect em = new GherkinDialectProvider().getDialect("em").orElseThrow(() -> new RuntimeException("Missing dialect: em"));
-        assertEquals(1, symbolCount(em.getScenarioKeywords().get(0)));
+        String actual = em.getScenarioKeywords().get(0);
+        assertEquals(1, actual.codePointCount(0, actual.length()));
     }
 
     @Test
