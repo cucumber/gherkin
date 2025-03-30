4c7269f040de66781e37321c6a1aace86d42a0b6
M.P. Korstanje <rien.korstanje@gmail.com>
2025-03-30 16:19:40 +0200

    Clean up

--- java/src/test/java/io/cucumber/gherkin/GherkinLineTest.java
+++ java/src/test/java/io/cucumber/gherkin/GherkinLineTest.java
@@ -16,7 +16,7 @@ class GherkinLineTest {
     final Location line = Locations.atLine(12);
 
     @Test
-    public void allows_any_non_space_characters_in_a_tag() {
+    void allows_any_non_space_characters_in_a_tag() {
         GherkinLine gherkinLine = new GherkinLine("   @foo:bar  @zap\uD83E\uDD52yo", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -28,7 +28,7 @@ class GherkinLineTest {
 
 
     @Test
-    public void finds_tags() {
+    void finds_tags() {
         GherkinLine gherkinLine = new GherkinLine("@this @is @a @tag", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -41,19 +41,19 @@ class GherkinLineTest {
     }
 
     @Test
-    public void throws_on_tags_with_spaces() {
+    void throws_on_tags_with_spaces() {
         GherkinLine gherkinLine = new GherkinLine("@this @is @a space separated @tag", line);
         assertThrows(ParserException.class, gherkinLine::getTags);
     }
 
     @Test
-    public void throws_on_tags_with_leading_spaces() {
+    void throws_on_tags_with_leading_spaces() {
         GherkinLine gherkinLine = new GherkinLine("@ leadingSpace", line);
         assertThrows(ParserException.class, gherkinLine::getTags);
     }
 
     @Test
-    public void ignores_empty_tag() {
+    void ignores_empty_tag() {
         GherkinLine gherkinLine = new GherkinLine("@", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -61,7 +61,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void ignores_empty_tags() {
+    void ignores_empty_tags() {
         GherkinLine gherkinLine = new GherkinLine("@@", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -69,7 +69,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_tags__trim_whitespace() {
+    void finds_tags__trim_whitespace() {
         GherkinLine gherkinLine = new GherkinLine("    @this @is  @a @tag  ", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -82,7 +82,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_tags__comment_inside_tag() {
+    void finds_tags__comment_inside_tag() {
         GherkinLine gherkinLine = new GherkinLine("@this @is #acomment  ", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -93,7 +93,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_tags__commented_before_tag() {
+    void finds_tags__commented_before_tag() {
         GherkinLine gherkinLine = new GherkinLine("@this @is #@a commented tag", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -104,7 +104,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_tags__commented_multiple_tags() {
+    void finds_tags__commented_multiple_tags() {
         GherkinLine gherkinLine = new GherkinLine("@this @is #@a @commented @sequence of tags", line);
         List<GherkinLineSpan> gherkinLineSpans = gherkinLine.getTags();
 
@@ -115,7 +115,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_table_cells() {
+    void finds_table_cells() {
         // The cells below has the following whitespace characters on each side:
         // - U+00A0 (non-breaking space)
         // - U+0020 (space)
@@ -142,7 +142,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void finds_escaped_table_cells() {
+    void finds_escaped_table_cells() {
         GherkinLine gherkinLine = new GherkinLine("      | \\|æ\\\\n     | \\o\\no\\  | \\\\\\|a\\\\\\\\n | ø\\\\\\nø\\\\|", line);
 
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
@@ -150,7 +150,7 @@ class GherkinLineTest {
     }
 
     @Test
-    public void preserve_escaped_new_lines_at_start_and_end() {
+    void preserve_escaped_new_lines_at_start_and_end() {
         GherkinLine gherkinLine = new GherkinLine("      |  \nraindrops--\nher last kiss\ngoodbye.\n  |", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("" +
@@ -162,42 +162,42 @@ class GherkinLineTest {
     }
 
     @Test
-    public void escapes_backslash() {
+    void escapes_backslash() {
         GherkinLine gherkinLine = new GherkinLine("|\\\\o\\no\\||", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("\\o\no|"), texts);
     }
 
     @Test
-    public void throws_on_illegal_escapes_backslash() {
+    void throws_on_illegal_escapes_backslash() {
         GherkinLine gherkinLine = new GherkinLine("|\\o\\no\\||", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("\\o\no|"), texts);
     }
 
     @Test
-    public void correctly_trims_white_spaces_before_cell_content() {
+    void correctly_trims_white_spaces_before_cell_content() {
         GherkinLine gherkinLine = new GherkinLine("|   \t spaces before|", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("spaces before"), texts);
     }
 
     @Test
-    public void correctly_trims_white_spaces_after_cell_content() {
+    void correctly_trims_white_spaces_after_cell_content() {
         GherkinLine gherkinLine = new GherkinLine("|spaces after   |", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("spaces after"), texts);
     }
 
     @Test
-    public void correctly_trims_white_spaces_around_cell_content() {
+    void correctly_trims_white_spaces_around_cell_content() {
         GherkinLine gherkinLine = new GherkinLine("|   \t spaces everywhere   \t|", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("spaces everywhere"), texts);
     }
 
     @Test
-    public void does_not_drop_white_spaces_inside_a_cell() {
+    void does_not_drop_white_spaces_inside_a_cell() {
         GherkinLine gherkinLine = new GherkinLine("| foo()\n  bar\nbaz |", line);
         List<String> texts = gherkinLine.getTableCells().stream().map(span -> span.text).collect(Collectors.toList());
         assertEquals(asList("foo()\n  bar\nbaz"), texts);
