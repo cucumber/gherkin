db711dd3598d59e2d2d43bbcf98a9d4a84670bed
Julien Kronegg <jk@kronegg.ch>
2025-04-04 12:54:12 +0000

    java: add unit tests and documen for Locations (#387)

--- java/src/main/java/io/cucumber/gherkin/Locations.java
+++ java/src/main/java/io/cucumber/gherkin/Locations.java
@@ -7,12 +7,13 @@ import static java.util.Objects.requireNonNull;
 class Locations {
 
     /**
-     * Cache of Long objects for the range 0-3999. This is used
+     * Cache of Long objects for the range 0-4095. This is used
      * to avoid creating a huge amount of Long objects in getLocation().
      * We can't use Long.valueOf() because it caches only the first 128
      * values, and typical feature files have much more lines.
      */
-    private final static Long[] longs = new Long[4000];
+    private static final Long[] longs = new Long[4096];
+
     static {
         for (int i = 0; i < longs.length; i++) {
             longs[i] = (long) i;
@@ -20,7 +21,22 @@ class Locations {
     }
 
     private static Long getLong(int i) {
-        if (i>=longs.length) return (long) i;
+        // JMH benchmark shows that this implementation is the
+        // fastest when i<4096 (and about 20% slower than
+        // Long.valueOf() when i>=4096).
+        //
+        // Tested variants:
+        // - static pre-initialized cache of 127 elements
+        //   (`Long.valueOf`)
+        // - static pre-initialized cache of 4096 elements
+        //   (the current implemented version)
+        // - dynamic pre-initialized cache with 256 / 512 /
+        //   1024/2048/4096 initial size
+        // - dynamic lazy initialized cache with 256
+        //   initialized size
+        if (i >= longs.length) {
+            return (long) i;
+        }
         return longs[i];
     }
 
@@ -33,6 +49,9 @@ class Locations {
     }
 
     static Location atLine(int line) {
+        if (line < 0) {
+            throw new IllegalArgumentException("Lines are index-0 based");
+        }
         return new Location(getLong(line), null);
     }
 
