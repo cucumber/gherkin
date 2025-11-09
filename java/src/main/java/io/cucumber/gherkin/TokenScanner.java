6c3a441c63e3851eae0f72eb9ff918e7cc443677
M.P. Korstanje <rien.korstanje@gmail.com>
2025-11-09 20:33:52 +0100

    Use String.lines(source).iterator()

--- java/src/main/java/io/cucumber/gherkin/TokenScanner.java
+++ java/src/main/java/io/cucumber/gherkin/TokenScanner.java
@@ -1,10 +1,9 @@
 package io.cucumber.gherkin;
 
 import io.cucumber.messages.types.Location;
+import org.jspecify.annotations.Nullable;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
+import java.util.Iterator;
 
 import static io.cucumber.gherkin.Token.createEOF;
 import static io.cucumber.gherkin.Token.createGherkinLine;
@@ -16,26 +15,21 @@ import static io.cucumber.gherkin.Token.createGherkinLine;
  */
 final class TokenScanner {
 
-    private final BufferedReader reader;
+    private final Iterator<String> reader;
     private int lineNumber;
 
     TokenScanner(String source) {
-        // TODO performance: replace BufferedReader by Java 11 String.lines(source).iterator() : 25-50% faster
-        this.reader = new BufferedReader(new StringReader(source));
+        this.reader = source.lines().iterator();
     }
 
     Token read() {
-        try {
-            String rawText = reader.readLine();
-            if (rawText == null) {
-                // Don't optimistically increment the line number
-                Location location = Locations.atLine(++lineNumber);
-                return createEOF(location);
-            }
+        if (reader.hasNext()) {
+            String rawText = reader.next();
             Location location = Locations.atLine(++lineNumber);
             return createGherkinLine(rawText, location);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        // Don't optimistically increment the line number
+        Location location = Locations.atLine(++lineNumber);
+        return createEOF(location);
     }
 }
