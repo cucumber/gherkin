8a90419714aad635f62fd4b93eac8edfda2abdfd
Kieran Ryan <kierankilkenny@gmail.com>
2026-01-07 22:55:26 +0000

    debt: prefer pathlib over os.path

--- python/tests/source_events_test.py
+++ python/tests/source_events_test.py
@@ -1,31 +1,17 @@
-from os import path
+from pathlib import Path
 
 from gherkin.stream.source_events import source_event
 
 
 def test_should_resolve_classic_feature():
     actual = source_event(
-        path.join(
-            path.dirname(__file__),
-            "..",
-            "..",
-            "testdata",
-            "good",
-            "minimal.feature",
-        ),
+        str(Path(__file__).parents[2] / "testdata/good/minimal.feature"),
     )
     assert actual["source"]["mediaType"] == "text/x.cucumber.gherkin+plain"
 
 
 def test_should_resolve_markdown_feature():
     actual = source_event(
-        path.join(
-            path.dirname(__file__),
-            "..",
-            "..",
-            "testdata",
-            "good",
-            "minimal.feature.md",
-        ),
+        str(Path(__file__).parents[2] / "testdata/good/minimal.feature.md"),
     )
     assert actual["source"]["mediaType"] == "text/x.cucumber.gherkin+markdown"
