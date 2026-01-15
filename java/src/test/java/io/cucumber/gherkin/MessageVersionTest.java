3c2cfbdc41b99e06a83112a4b8276fab0a192941
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-15 21:25:50 +0100

    Reapply "Introduce module"
    This reverts commit f31c5c297e292f5d95d763b9d9a3fd5d3a8d9256.

--- java/src/test/java/io/cucumber/gherkin/MessageVersionTest.java
+++ java/src/test/java/io/cucumber/gherkin/MessageVersionTest.java
@@ -3,13 +3,14 @@ package io.cucumber.gherkin;
 import io.cucumber.messages.types.Envelope;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 // This tests that the message library exposes its version. This test is hard to do in the
 // library itself since it requires running against a packaged version (jar).
 class MessageVersionTest {
     @Test
     void message_library_has_version() {
-        assertNotNull(Envelope.class.getPackage().getImplementationVersion());
+        var version = Envelope.class.getModule().getDescriptor().version();
+        assertThat(version).isNotEmpty();
     }
 }
