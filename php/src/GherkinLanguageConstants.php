f335787cfd20defddd0fd5040aa936e8d1280ff1
M.P. Korstanje <rien.korstanje@gmail.com>
2026-02-05 21:28:09 +0100

    Remove constant type delcaration

--- php/src/GherkinLanguageConstants.php
+++ php/src/GherkinLanguageConstants.php
@@ -4,10 +4,10 @@ namespace Cucumber\Gherkin;
 
 interface GherkinLanguageConstants
 {
-    public const string TAG_PREFIX = "@";
-    public const string COMMENT_PREFIX = "#";
-    public const string TITLE_KEYWORD_SEPARATOR = ":";
-    public const string TABLE_CELL_SEPARATOR = "|";
-    public const string DOCSTRING_SEPARATOR = "\"\"\"";
-    public const string DOCSTRING_ALTERNATIVE_SEPARATOR = "```";
+    public const TAG_PREFIX = "@";
+    public const COMMENT_PREFIX = "#";
+    public const TITLE_KEYWORD_SEPARATOR = ":";
+    public const TABLE_CELL_SEPARATOR = "|";
+    public const DOCSTRING_SEPARATOR = "\"\"\"";
+    public const DOCSTRING_ALTERNATIVE_SEPARATOR = "```";
 }
