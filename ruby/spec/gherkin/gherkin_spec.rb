7347ffeb2aeba498ed8d1d6e380310c20fce5f75
Luke Hill <lukehill_uk@hotmail.com>
2026-04-06 18:45:14 +0100

    Tidy up gherkin spec

--- ruby/spec/gherkin/gherkin_spec.rb
+++ ruby/spec/gherkin/gherkin_spec.rb
@@ -1,42 +1,24 @@
 # frozen_string_literal: true
 
 describe Gherkin do
+  let(:options) { { include_source: true, include_gherkin_document: true, include_pickles: true } }
+
   it 'can process feature file paths' do
-    messages = Gherkin.from_paths(
-      ['../testdata/good/minimal.feature'],
-      { include_source: true,
-        include_gherkin_document: true,
-        include_pickles: true }
-    ).to_a
+    messages = Gherkin.from_paths(['../testdata/good/minimal.feature'], options).to_a
 
     expect(messages.length).to eq(3)
   end
 
   it 'can process feature file content' do
     data = File.open('../testdata/good/minimal.feature', 'r:UTF-8', &:read)
-
-    messages = Gherkin.from_source(
-      'uri',
-      data,
-      { include_source: true,
-        include_gherkin_document: true,
-        include_pickles: true }
-    ).to_a
+    messages = Gherkin.from_source('uri', data, options).to_a
 
     expect(messages.length).to eq(3)
   end
 
   it 'can set the default dialect for the feature file content' do
     data = File.open('../testdata/good/i18n_no.feature', 'r:UTF-8', &:read)
-
-    messages = Gherkin.from_source(
-      'uri',
-      data,
-      { include_source: true,
-        include_gherkin_document: true,
-        include_pickles: true,
-        default_dialect: 'no' }
-    ).to_a
+    messages = Gherkin.from_source('uri', data, options.merge(default_dialect: 'no')).to_a
 
     expect(messages.length).to eq(3)
   end
