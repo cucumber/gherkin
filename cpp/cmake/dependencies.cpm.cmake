d698e8d1d14103dca12cfd5184120caf3208c213
Timmer, Daan <daan.timmer+github@gmail.com>
2026-06-18 06:41:58 +0000

    fix: update cucumber_messages GIT_TAG to latest commit

--- cpp/cmake/dependencies.cpm.cmake
+++ cpp/cmake/dependencies.cpm.cmake
@@ -32,7 +32,7 @@ CPMAddPackage(
 CPMAddPackage(
     NAME cucumber_messages
     GITHUB_REPOSITORY cucumber/messages
-    GIT_TAG 089479c092df4e10bb86d4413094f920858fc48e #cpp-remove-cmate-use-native-cmake-and-CPM
+    GIT_TAG 572d419f3ba14bbeb7123714a9e4f74698bc3363 #cpp-remove-cmate-use-native-cmake-and-CPM
     SOURCE_SUBDIR cpp
     SYSTEM
 )
