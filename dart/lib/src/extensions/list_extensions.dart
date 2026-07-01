8b79dbeca974ec9803d562fdca79ee5576957b5a
Andrew Friedman <andrewfriedman01@gmail.com>
2026-07-01 08:49:01 -0400

    Remove ListExtension from list_extensions.dart
    - Deleted the `ListExtension` extension from `list_extensions.dart`, which provided convenience accessors for lists. This change simplifies the codebase by removing unused functionality.

--- dart/lib/src/extensions/list_extensions.dart
+++
@@ -1,5 +0,0 @@
-/// Convenience accessors for lists.
-extension ListExtension<T> on List<T> {
-  /// Returns the first element, or [defaultResult] if the list is empty.
-  T singleOrDefault(T defaultResult) => isNotEmpty ? first : defaultResult;
-}
