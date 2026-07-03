c90e7ffe860c7992ea75be36116ab9ead4df4898
Timmer, Daan <daan.timmer@philips.com>
2026-07-03 14:17:41 +0000

    refactor: remove CPM dependency management and streamline package inclusion

--- cpp/cmake/dependencies.cpm.cmake
+++ cpp/cmake/dependencies.cpm.cmake
@@ -1,8 +1,9 @@
 # ---------------------------------------------------------------------------
 # CPM – download on first configure if not already cached (standalone only)
 # ---------------------------------------------------------------------------
-if(CUCUMBER_GHERKIN_FETCH_DEPS)
+if(NOT COMMAND CPMAddPackage)
     set(CPM_DOWNLOAD_VERSION 0.40.2)
+    set(CPM_USE_LOCAL_PACKAGES ON)
     set(CPM_DOWNLOAD_LOCATION
         "${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 
@@ -29,10 +30,23 @@ CPMAddPackage(
     SYSTEM
 )
 
+if(TARGET nlohmann_json)
+    set_target_properties(nlohmann_json PROPERTIES SYSTEM ON)
+endif()
+
+if(nlohmann_json_ADDED)
+    # Make the generated config file discoverable by cucumber_messages' find_package()
+    list(APPEND CMAKE_PREFIX_PATH "${nlohmann_json_BINARY_DIR}")
+    if(NOT cucumber_messages_MAIN_PROJECT)
+        set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" PARENT_SCOPE)
+    endif()
+endif()
+
+
 CPMAddPackage(
     NAME cucumber_messages
     GITHUB_REPOSITORY cucumber/messages
-    GIT_TAG 572d419f3ba14bbeb7123714a9e4f74698bc3363 #cpp-remove-cmate-use-native-cmake-and-CPM
+    GIT_TAG c34b8cf6412f274ac6943075326440b7e6062c81 #refactor-cpp-codegen
     SOURCE_SUBDIR cpp
     SYSTEM
 )
