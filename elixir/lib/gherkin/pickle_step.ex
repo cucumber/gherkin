f1f24345f7433c805ca0e88c744fcf1fc89c34ed
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-07 18:07:51 +0200

    elixir: Remove unused structs

--- elixir/lib/gherkin/pickle_step.ex
+++
@@ -1,9 +0,0 @@
-defmodule CucumberGherkin.PickleStep do
-  @moduledoc false
-
-  defstruct argument: nil,
-            ast_node_ids: [],
-            id: "",
-            text: "",
-            type: "Unknown"
-end
