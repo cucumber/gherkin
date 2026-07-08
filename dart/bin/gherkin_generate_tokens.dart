6ff79d1b9f58c900968394875e8a17098ca47216
Andrew Friedman <andrewfriedman01@gmail.com>
2026-07-07 22:57:22 -0400

    Migrate tool/ to bin

--- dart/bin/gherkin_generate_tokens.dart
+++ dart/bin/gherkin_generate_tokens.dart
@@ -4,7 +4,7 @@ import 'package:cucumber_gherkin/src/language/dialects_builtin.g.dart';
 import 'package:cucumber_gherkin/src/language/gherkin_dialect_provider.dart';
 import 'package:cucumber_gherkin/src/language/gherkin_token_matcher.dart';
 
-import '../tool/tokens_generator.dart';
+import 'tokens_generator.dart';
 
 /// Emits the tokenized representation of one or more Gherkin feature files,
 /// matching the `*.tokens` format used by the acceptance tests.
