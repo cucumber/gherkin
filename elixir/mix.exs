ed7b88663de2ca6c5e0ea3db5b6574350ade71c5
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-19 15:40:23 +0200

    chore(deps): update dependency ex_doc to ~> 0.40.3

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -28,7 +28,7 @@ defmodule CucumberGherkin.MixProject do
     [
       {:elixir_uuid, "~> 1.2"},
       {:cucumber_messages, ">= 34.0.0 and < 35.0.0"},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40.3", only: :dev, runtime: false}
     ]
   end
 
